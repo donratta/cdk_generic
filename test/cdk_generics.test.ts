7fc9c13b5f4443728dfcee9626183fcd5ef5cc5e
Abdulrahman Atta <abdulrahman.atta0016@gmail.com>
2023-05-22 10:16:46 -0400

    Initial commit

---
+++ test/cdk_generics.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkGenerics from '../lib/cdk_generics-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_generics-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkGenerics.CdkGenericsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
